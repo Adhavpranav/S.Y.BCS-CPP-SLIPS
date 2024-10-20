0b55a5717c2f995d3cf7484f55f855ef6a6f55fb
Pranav Adhav <142861984+Adhavpranav@users.noreply.github.com>
2024-10-20 18:40:02 +0530

    Create slip5.cpp

---
+++ CPP-15-MARKS-PROGRAMS/slip5.cpp
@@ -0,0 +1,77 @@
+/*
+Write a C++ program to create two classes Array1 and Array2 with an integer array as a 
+data member. Write necessary member functions to accept and display array elements of 
+both the classes. Find and display maximum of both the array. (Use Friend function).  
+*/
+#include<iostream>
+using namespace std;
+class Array2;
+class Array1
+{
+    private:
+        int array[100];
+        public:
+        void accept(int limit)
+        {
+            cout<<"ENTER FIRST ARRAY ELEMENTS:";
+            for(int i=0;i<limit;i++)
+            {
+                cin>>array[i];
+            }
+        }
+        void display(int limit)
+        {
+            cout<<endl<<"FIRST ARRAY:";
+            for(int i=0;i<limit;i++)
+            cout<<" "<<array[i];
+        }
+        friend void maximum(Array1 ob1,Array2 ob2,int limit);
+};
+class Array2
+{
+    private:
+        int array[100];
+        public:
+        void accept(int limit)
+        {
+            cout<<"ENTER SECOND ARRAY ELEMENTS:";
+            for(int i=0;i<limit;i++)
+            {
+                cin>>array[i];
+            }
+        }
+        void display(int limit)
+        {
+            cout<<endl<<"SECOND ARRAY:";
+            for(int i=0;i<limit;i++)
+            cout<<" "<<array[i];
+        }
+        friend void maximum(Array1 ob1,Array2 ob2,int limit);
+};
+
+        void maximum(Array1 ob1,Array2 ob2,int limit)
+        {
+            int max;
+            max=ob1.array[0];
+            for(int i=1;i<limit;i++)
+            if(max<ob1.array[i])
+            max=ob1.array[i];
+            for(int i=0;i<limit;i++)
+            if(max<ob2.array[i])
+            max=ob2.array[i];
+            cout<<endl<<"MAXIMUM NUMBER IS:"<<max;
+        }
+
+int main()
+{
+    int limit;
+    cout<<"ENTER A ARRAY LIMIT:";
+    cin>>limit;
+    Array1 ob1;
+    Array2 ob2;
+    ob1.accept(limit);
+    ob2.accept(limit);
+    ob1.display(limit);
+    ob2.display(limit);
+    maximum(ob1,ob2,limit);
+}
